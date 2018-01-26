bd0167c2e8f8e839e42bbb0caeb1e3f04c744a70
Carl Olson <colson10@gmail.com>
2018-01-25 16:54:41 -0800

    added function for page load that has if statement to determine which page, parses local storage on cart page and loads items selected

--- app.js
+++ app.js
@@ -4,13 +4,20 @@
 // with each iteration create a option element. give it the value of i in the array.
 //append to the selectEL.
 
+// if (sectionEl) {
+
+// }
+
 
 Product.allProduct = [];
+var selectedObjects = [];
+var itemAmountValues = [];
 
 var selectEl = document.getElementById('drop-down');
 var divEl = document.getElementById('message');
-var buttonEl = document.getElementById('add-product');
 var formEl = document.getElementById('products-form');
+var pEL = document.getElementById('alert-message');
+var sectionEl = document.getElementById('cart-items');
 
 
 function Product(filepath, name) {
@@ -42,34 +49,93 @@ new Product('img/usb.png', 'usb');
 new Product('img/water-can.jpg', 'water-can');
 new Product('img/wine-glass.jpg', 'wine-glass');
 
+
+function buttonHandler(event) {
+  event.preventDefault();
+  console.log(event.target.itemSelected.value);
+  console.log(event.target.amount.valueAsNumber);
+
+  var itemName = event.target.itemSelected.value;
+  var itemAmount = event.target.amount.valueAsNumber;
+
+  // run a loop through Product.allProduct to see if itemName matches Product.allProduct[i].name
+
+  for (var i in Product.allProduct) {
+    if (Product.allProduct[i].name === itemName) {
+      selectedObjects.push(Product.allProduct[i]);
+      console.log(Product.allProduct[i]);
+      break;
+    }
+  }
+
+  // itemSelectedNames.push(itemName);
+  itemAmountValues.push(itemAmount);
+
+  localStorage.setItem('storedObjects', JSON.stringify(selectedObjects));
+  localStorage.setItem('storedAmounts', JSON.stringify(itemAmountValues));
+
+
+  pEL.textContent = 'Item(s) added to Cart!';
+  // console.log(pEL.textContent);
+  divEl.appendChild(pEL);
+  
+}
+
+
+
 function populateDropDown() {
+  formEl.addEventListener('submit', buttonHandler);
   var optionEL = document.createElement('option');
-  for (i in Product.allProduct) {
+  for (var i in Product.allProduct) {
     optionEL = document.createElement('option');
     optionEL.textContent = Product.allProduct[i].name;
     selectEl.appendChild(optionEL);
   }
 }
 
+function displayCartItems() {
+  selectedObjects = JSON.parse(localStorage.storedObjects);
+  itemAmountValues = JSON.parse(localStorage.storedAmounts);
+  var h4El = document.createElement('h4');
+  var h5El = document.createElement('h5');
+  var imgEl = document.createElement('img');
+  for (var i in selectedObjects) {
+    h4El = document.createElement('h4');
+    h5El = document.createElement('h5');
+    imgEl = document.createElement('img');
 
-function buttonHandler(event) {
-  console.log("hello");
-  event.preventDefault();
-  console.log(event.target.children);
-  
+    h4El.textContent = selectedObjects[i].name;
+    h5El.textContent = itemAmountValues[i];
+    imgEl.src = selectedObjects[i].filepath;
 
-
-  var pEL = document.createElement('p');
-  pEL.textContent = ' Item added to Cart!';
-  // console.log(pEL.textContent);
-  divEl.appendChild(pEL);
+    sectionEl.appendChild(h4El);
+    sectionEl.appendChild(h5El);
+    sectionEl.appendChild(imgEl);
+  }
 }
 
-formEl.addEventListener('submit', buttonHandler);
+
+function pageLoad() {
+  if (sectionEl) {
+    displayCartItems();
+  } else {
+    populateDropDown();
+  }
+}
+
+
+
+
 
 // Grab button using the DOM
 // create an event listern on it that runa a function buttonHandler
 // buttonHandler will create an element using the DOM and giev it content. Display message.
 
 
-populateDropDown();
+// run a function on page load that checks if it's index.html or cart.html 
+// if index.html, run populateDropDown, if cart.html run displayCartItems
+
+
+pageLoad();
+// populateDropDown();
+// displayCartItems();
